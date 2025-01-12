a92dacb3672603b3250fe972720d4f815779385b
surendra <surendra.mishra100896@gmail.com>
2025-01-12 11:58:02 +0530

    fixed the show my order api

--- backend/ecom_product/admin.py
+++ backend/ecom_product/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import ProductCategory, Product, ProductImage, Cart, CartItem
+from .models import ProductCategory, Product, ProductImage, Cart, CartItem, UserAddress
 
 admin.site.register(ProductCategory)
 admin.site.register(Product)
 admin.site.register(ProductImage)
 admin.site.register(Cart)
 admin.site.register(CartItem)
+admin.site.register(UserAddress)
